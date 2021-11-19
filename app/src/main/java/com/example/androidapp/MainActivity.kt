355102ae12e1373b21f902766d7bd9c6e4dc5838
Alveiro Bueno <alveirobueno@gmail.com>
2021-11-18 20:09:27 -0500

    Jueves Noche

--- app/src/main/java/com/example/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.androidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_window1)
+
+        val recycler = findViewById<RecyclerView>(R.id.recycler)
+        recycler.adapter
     }
 }
---
+++ app/src/main/java/com/example/androidapp/SitesAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.androidapp
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class SitesAdapter : RecyclerView.Adapter<SitesAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SitesAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: SitesAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view)
+}
