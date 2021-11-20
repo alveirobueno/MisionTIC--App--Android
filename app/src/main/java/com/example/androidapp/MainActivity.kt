a16af48f438976474a4130703530b077660342cb
Alveiro Bueno <alveirobueno@gmail.com>
2021-11-20 10:02:02 -0500

    OK Recycler

--- app/src/main/java/com/example/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -2,14 +2,33 @@ package com.example.androidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+
+//    ArrayList<String> cities = new ArrayList<>();
+//    ArrayList<String> imageCities = new ArrayList<>();
+//    ArrayList<String> infoCities = new ArrayList<>();
+//    ArrayList<String> punctCities = new ArrayList<>();
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_window1)
+        this.supportActionBar?.hide()
+        setContentView(R.layout.activity_main)
 
         val recycler = findViewById<RecyclerView>(R.id.recycler)
-        recycler.adapter
+        val adapter = SitesAdapter()
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.adapter = adapter
+
+//        JSONObject obj = new JSONObject(loadJSONfromAssets());
+
+
+    }
+
+    private fun loadJSONfromAssets(): String {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/example/androidapp/SitesAdapter.kt
+++ app/src/main/java/com/example/androidapp/SitesAdapter.kt
@@ -1,22 +1,71 @@
 package com.example.androidapp
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+
+import java.io.InputStream
+
+import org.json.JSONObject
+
 
 class SitesAdapter : RecyclerView.Adapter<SitesAdapter.ViewHolder>() {
 
+    private var cityText = arrayOf(
+        "Ciudad 1",
+        "Cuudad 2",
+        "Ciudad 3",
+        "Ciudad 4",
+        "Ciudad 5")
+
+    private var imageFiles = intArrayOf(
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground)
+
+    private var shortInfoText = arrayOf(
+        "Info 1",
+        "Info 2",
+        "Info 3",
+        "Info 4",
+        "Info 5")
+
+    private var punctuationText = arrayOf(
+        "Puntuación 1",
+        "Puntuación 2",
+        "Puntuación 3",
+        "Puntuación 4",
+        "Puntuación 5")
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SitesAdapter.ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.activity_item_ok, parent, false)
+        return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: SitesAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.cityName.text = cityText[position]
+        viewHolder.image.setImageResource(imageFiles[position])
+        viewHolder.shortInfo.text = shortInfoText[position]
+        viewHolder.punctuation.text = punctuationText[position]
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun getItemCount(): Int = cityText.size
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+        val cityName = itemView.findViewById<TextView>(R.id.titleList)
+        val image = itemView.findViewById<ImageView>(R.id.imageList)
+        val shortInfo = itemView.findViewById<TextView>(R.id.textList)
+        val punctuation = itemView.findViewById<TextView>(R.id.textPunct)
+
     }
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view)
 }
